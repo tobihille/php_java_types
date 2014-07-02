1b711a40eb3197faae90734e1a862bb1afdc8c94
Tobias Hille <tobihille@googlemail.com>
2014-07-02 21:40:47 +0200

    completed typeLoader

--- typeLoader.php
+++ typeLoader.php
@@ -11,5 +11,8 @@ if ( !endsWith($filePath, DIRECTORY_SEPARATOR))
     $filePath .= '/';
 }
 
+require_once($filePath.'ConvBool.php');
+require_once($filePath.'ConvChar.php');
+require_once($filePath.'ConvInt.php');
 require_once($filePath.'ConvString.php');
 require_once($filePath.'misc.php');
